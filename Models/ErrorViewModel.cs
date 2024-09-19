768c6f11971bca5a72d10d34a41d970cd69e1699
rohini.lokare <rohini.lokare@powerweave.com>
2024-09-19 13:53:48 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RohiniASP.NetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
